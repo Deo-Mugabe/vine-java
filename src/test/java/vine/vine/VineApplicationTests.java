c0221e4fdf989f0854b18138961ca690aca1a507
Deo_Mugabe <degra9@gmail.com>
2025-06-22 15:59:56 -0500

    initial setup

---
+++ src/test/java/vine/vine/VineApplicationTests.java
@@ -0,0 +1,13 @@
+package vine.vine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
